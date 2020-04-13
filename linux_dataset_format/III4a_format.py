69790c22e6da1789d2598f672f882e1a7b91730e
Vitor Vilas Boas <vitormendesvilasboas@gmail.com>
2020-04-13 20:35:33 -0300

    update format files

--- linux_dataset_format/III4a_format.py
+++ linux_dataset_format/III4a_format.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-
-from datetime import datetime
-from scipy.io import loadmat
-import numpy as np
-import pickle
-
+"""
+Created on Sat Mar 14 09:44:10 2020
+@author: Vitor Vilas-Boas
+"""
 """ 5 subjects | 2 classes (RH, FooT)
     Epoch distribution:
         aa : train=168 test=112  
@@ -15,25 +13,33 @@ import pickle
     Start trial= 0; Start cue=0; Start MI= 0; End MI=3.5; End trial(break)= 5.25~5.75
 """
 
-path = '/mnt/dados/eeg_data/III4a/' ## >>> SET HERE THE DATA SET PATH
+import os
+import pickle
+import numpy as np
+from datetime import datetime
+from scipy.io import loadmat
+
+path = '/mnt/dados/eeg_data/III4a/mat/' ## >>> SET HERE THE DATA SET PATH
 
 for suj in ['aa','al','av','aw','ay']:
-    mat = loadmat(path + 'mat/' + suj + '.mat')
-    data = mat['cnt'].T # 0.1 * mat['cnt'].T # convert to uV
+    mat = loadmat(path + suj + '.mat')
+    d = mat['cnt'].T # 0.1 * mat['cnt'].T # convert to uV
     pos = mat['mrk'][0][0][0][0]
-    true_mat = loadmat(path + 'mat/true_labels/trues_' + suj + '.mat')
+    true_mat = loadmat(path + 'true_labels_' + suj + '.mat')
     true_y = np.ravel(true_mat['true_y']) # RH=1 Foot=2
     true_y = np.where(true_y == 2, 3, true_y) # RH=1 Foot=3
-    # true_test_idx = np.ravel(true_mat['test_idx'])
     events = np.c_[pos, true_y]
-    # data = corrigeNaN(data)
-    # data = np.asarray([ np.nan_to_num(dt) for dt in data ])
-    # data = np.asarray([ np.ravel(pd.DataFrame(dt).fillna(pd.DataFrame(dt).mean())) for dt in data ])
-    info = {'fs': 100, 'class_ids': [1, 3], 'trial_tcue': 0,
-            'trial_tpause': 4.0, 'trial_mi_time': 4.0, 'trials_per_class': 140,
-            'eeg_channels': 118, 'ch_labels': mat['nfo']['clab'],
-            'datetime': datetime.now().strftime('%d-%m-%Y_%Hh%Mm')}
-    
+    # d = corrigeNaN(d)
+    # d = np.asarray([ np.nan_to_num(j) for j in d ])
+    # d = np.asarray([ np.ravel(pd.DataFrame(j).fillna(pd.DataFrame(j).mean())) for j in d ])
 
-    np.save(path + 'npy/' + suj, [data, events, info], allow_pickle=True)
-    # with open(path + 'omi/' + suj + '.omi', 'wb') as handle: pickle.dump([data, events, info], handle)
+    i = {'fs':100, 'class_ids':[1, 3], 'trial_tcue':0, 'trial_tpause':4.0, 'trial_mi_time':4.0,
+         'trials_per_class':140, 'eeg_channels':d.shape[0], 'ch_labels':mat['nfo']['clab'],
+         'datetime':datetime.now().strftime('%d-%m-%Y_%Hh%Mm')}
+
+    path_out = path + '../npy/'
+    if not os.path.isdir(path_out): os.makedirs(path_out)
+
+    #%% save npy file
+    np.save(path_out + suj, [d, e, i], allow_pickle=True)
+    # pickle.dump([data, events, info], open(path_out + suj + '.omi', 'wb'))
