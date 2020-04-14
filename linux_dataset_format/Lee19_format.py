1f8a8ba4a8442f3f27b32fb087c802d97bed40d4
Vitor Vilas Boas <vitormendesvilasboas@gmail.com>
2020-04-13 21:51:45 -0300

    update format files

--- linux_dataset_format/Lee19_format.py
+++ linux_dataset_format/Lee19_format.py
@@ -77,7 +77,7 @@ for suj in range(1, 5):
     e2[:, 1] = np.where(e2[:, 1] == 2, 1, 2)
 
     info = {'fs': Fs, 'class_ids': [1, 2], 'trial_tcue': 3.0, 'trial_tpause': 7.0,
-            'trial_mi_time': 4.0, 'trials_per_class': 50, 'eeg_channels': d1.shape[0],
+            'trial_mi_time': 4.0, 'trials_per_class': 100, 'eeg_channels': d1.shape[0],
             'ch_labels': list(['Fp1', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'FC5', 'FC1', 'FC2', 'FC6', 'T7', 'C3', 'Cz',
                                 'C4', 'T8', 'TP9', 'CP5', 'CP1', 'CP2', 'CP6', 'TP10', 'P7', 'P3', 'Pz', 'P4', 'P8', 'PO9',
                                 'O1', 'Oz', 'O2', 'PO10', 'FC3', 'FC4', 'C5', 'C1', 'C2', 'C6', 'CP3', 'CPz', 'CP4', 'P1',
