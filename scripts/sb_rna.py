0259d6fb6736206ec89a5009b00c09089cd43e42
Vitor Vilas Boas <vitormendesvilasboas@gmail.com>
2020-06-28 22:58:50 -0300

    update sb_rna.py

--- scripts/sb_rna.py
+++ scripts/sb_rna.py
@@ -46,6 +46,7 @@ def extractEpochs(data, events, smin, smax, class_ids):
             bad_epoch_list.append(i)
     labels = np.delete(labels, bad_epoch_list)
     epochs = np.delete(epochs, bad_epoch_list, axis=0)
+    epochs = nanCleaner(epochs)
     return epochs, labels
 
 
@@ -131,36 +132,36 @@ if __name__ == "__main__":
     smin, smax = int(tmin*Fs), int(tmax*Fs)
     
     ### to gdf file
-    # eeg = mne.io.read_raw_gdf(path_gdf + str(suj) + 'T.gdf').load_data()
-    # data1 = eeg.get_data()[:22] # [channels x samples]
-    # events1 = mne.events_from_annotations(eeg) # raw.find_edf_events()
+    eeg = mne.io.read_raw_gdf(path_gdf + str(suj) + 'T.gdf').load_data()
+    data1 = eeg.get_data()[:22] # [channels x samples]
+    events1 = mne.events_from_annotations(eeg) # raw.find_edf_events()
     
-    # eeg = mne.io.read_raw_gdf(path_gdf + str(suj) + 'E.gdf').load_data()
-    # data2 = eeg.get_data()[:22] # [channels x samples]
-    # events2 = mne.events_from_annotations(eeg) # raw.find_edf_events()
+    eeg = mne.io.read_raw_gdf(path_gdf + str(suj) + 'E.gdf').load_data()
+    data2 = eeg.get_data()[:22] # [channels x samples]
+    events2 = mne.events_from_annotations(eeg) # raw.find_edf_events()
     
-    # # for k,v in events1[1].items(): print(f'{k}:: {v}')
-    # # for k,v in events2[1].items(): print(f'{k}:: {v}')
-    # lb_utils = [4,5,6,7]
+    # for k,v in events1[1].items(): print(f'{k}:: {v}')
+    # for k,v in events2[1].items(): print(f'{k}:: {v}')
+    lb_utils = [4,5,6,7]
     
-    # events1 = np.delete(events1[0], 1, axis=1)
-    # events2 = np.delete(events2[0], 1, axis=1)
+    events1 = np.delete(events1[0], 1, axis=1)
+    events2 = np.delete(events2[0], 1, axis=1)
     
-    # ZT, tt = extractEpochs(data1, events1, smin, smax, lb_utils)
-    # for i,k in zip(lb_utils, range(1, 5)): tt = np.where(tt == i, k, tt)
-    # ZT = np.vstack([ ZT[np.where(tt == k)] for k in class_ids ])
-    # tt = np.hstack([ np.ones(len(ZT)//2)*k for k in class_ids ]).astype(int) 
+    ZT, tt = extractEpochs(data1, events1, smin, smax, lb_utils)
+    for i,k in zip(lb_utils, range(1, 5)): tt = np.where(tt == i, k, tt)
+    ZT = np.vstack([ ZT[np.where(tt == k)] for k in class_ids ])
+    tt = np.hstack([ np.ones(len(ZT)//2)*k for k in class_ids ]).astype(int) 
     
-    # ZV, _ = extractEpochs(data2, events2, smin, smax, [4])
-    # tv = np.ravel(loadmat(path_trues + str(suj) + 'E.mat' )['classlabel'])
-    # ZV = np.vstack([ ZV[np.where(tv == k)] for k in class_ids ])
-    # tv = np.hstack([ np.ones(len(ZV)//2)*k for k in class_ids ]).astype(int) 
+    ZV, _ = extractEpochs(data2, events2, smin, smax, [4])
+    tv = np.ravel(loadmat(path_trues + str(suj) + 'E.mat' )['classlabel'])
+    ZV = np.vstack([ ZV[np.where(tv == k)] for k in class_ids ])
+    tv = np.hstack([ np.ones(len(ZV)//2)*k for k in class_ids ]).astype(int) 
 
     ### to npy file
-    data1, events1, _  = np.load('/mnt/dados/eeg_data/IV2a/npy/A0' + str(suj) + 'T.npy', allow_pickle=True) # sessão 1
-    data2, events2, _  = np.load('/mnt/dados/eeg_data/IV2a/npy/A0' + str(suj) + 'E.npy', allow_pickle=True) # sessão 2
-    ZT, tt = extractEpochs(data1, events1, smin, smax, class_ids)
-    ZV, tv = extractEpochs(data2, events2, smin, smax, class_ids)
+    # data1, events1, _  = np.load('/mnt/dados/eeg_data/IV2a/npy/A0' + str(suj) + 'T.npy', allow_pickle=True) # sessão 1
+    # data2, events2, _  = np.load('/mnt/dados/eeg_data/IV2a/npy/A0' + str(suj) + 'E.npy', allow_pickle=True) # sessão 2
+    # ZT, tt = extractEpochs(data1, events1, smin, smax, class_ids)
+    # ZV, tv = extractEpochs(data2, events2, smin, smax, class_ids)
     
     step = (fh-fl) / (nbands+1) # n_bins/setup['nbands']+1
     size = step / 0.5 # step/overlap
@@ -225,91 +226,90 @@ if __name__ == "__main__":
     print('MLP acc:', round(mlp.score(META_SV, TV)*100,2), '\n')
     
     
+    # =============================================================================
+    #     #FULL TRIAL 
+    # =============================================================================
+    smin, smax = int(-2*Fs), int(4*Fs)
     
+    ### to gdf file
+    trialsT, labelsT = extractEpochs(data1, events1, smin, smax, lb_utils)
+    for i,k in zip(lb_utils, range(1, 5)): labelsT = np.where(labelsT == i, k, labelsT)
+    trialsT = np.vstack([ trialsT[np.where(labelsT == k)] for k in class_ids ])
+    labelsT = np.hstack([ np.ones(len(trialsT)//2)*k for k in class_ids ]).astype(int) 
     
+    trialsV, _ = extractEpochs(data2, events2, smin, smax, [4])
+    labelsV = np.ravel(loadmat(path_trues + str(suj) + 'E.mat' )['classlabel'])
+    trialsV = np.vstack([ trialsV[np.where(labelsV == k)] for k in class_ids ])
+    labelsV = np.hstack([ np.ones(len(trialsV)//2)*k for k in class_ids ]).astype(int) 
     
-    ########## FULL TRIAL ###############################
-    # smin, smax = int(-2*Fs), int(4*Fs)
-    
-    # ### to gdf file
-    # # trialsT, labelsT = extractEpochs(data1, events1, smin, smax, lb_utils)
-    # # for i,k in zip(lb_utils, range(1, 5)): labelsT = np.where(labelsT == i, k, labelsT)
-    # # trialsT = np.vstack([ trialsT[np.where(labelsT == k)] for k in class_ids ])
-    # # labelsT = np.hstack([ np.ones(len(trialsT)//2)*k for k in class_ids ]).astype(int) 
-    
-    # # trialsV, _ = extractEpochs(data2, events2, smin, smax, [4])
-    # # labelsV = np.ravel(loadmat(path_trues + str(suj) + 'E.mat' )['classlabel'])
-    # # trialsV = np.vstack([ trialsV[np.where(labelsV == k)] for k in class_ids ])
-    # # labelsV = np.hstack([ np.ones(len(trialsV)//2)*k for k in class_ids ]).astype(int) 
-    
-    # ### to npy file
+    ### to npy file
     # data1, events1, _  = np.load('/mnt/dados/eeg_data/IV2a/npy/A0' + str(suj) + 'T.npy', allow_pickle=True) # sessão 1
     # data2, events2, _  = np.load('/mnt/dados/eeg_data/IV2a/npy/A0' + str(suj) + 'E.npy', allow_pickle=True) # sessão 2
     # trialsT, labelsT = extractEpochs(data1, events1, smin, smax, class_ids)
     # trialsV, labelsV = extractEpochs(data2, events2, smin, smax, class_ids)
     
-    # delta_t = 0.2; delta_s = int(delta_t * Fs)
-    # q = int((tmax*Fs)-(tmin*Fs))  # q = largura em amostras
-    # ZT, tt, ZV, tv = [], [], [], []
-    # for k in range(len(trialsT)): # len(trials_cal)
-    #     n = q   # n=localização em amostras (fim)
-    #     m = q/2 # localização em amostra do ponto médio da época
-    #     inc = (0.0 * q)  # para ser considerado comando, no min 70% da amostra (m+10%) deve estar contida no periodo de MI
-    #     while n <= trialsT.shape[-1]:
-    #         ZT.append(trialsT[k, :, n-q:n])
-    #         ZV.append(trialsV[k, :, n-q:n])
-    #         if (m <= (500+inc)) or (m >= (1500-inc)): tt.append(0); tv.append(0); 
-    #         else: tt.append(labelsT[k]); tv.append(labelsV[k])
-    #         m += delta_s
-    #         n += delta_s
-    # ZT, ZV = np.asarray(ZT), np.asarray(ZV)
-    # tt, tv = np.asarray(tt), np.asarray(tv)
+    delta_t = 0.2; delta_s = int(delta_t * Fs)
+    q = int((tmax*Fs)-(tmin*Fs))  # q = largura em amostras
+    ZT, tt, ZV, tv = [], [], [], []
+    for k in range(len(trialsT)): # len(trials_cal)
+        n = q   # n=localização em amostras (fim)
+        m = q/2 # localização em amostra do ponto médio da época
+        inc = (0.0 * q)  # para ser considerado comando, no min 70% da amostra (m+10%) deve estar contida no periodo de MI
+        while n <= trialsT.shape[-1]:
+            ZT.append(trialsT[k, :, n-q:n])
+            ZV.append(trialsV[k, :, n-q:n])
+            if (m <= (500+inc)) or (m >= (1500-inc)): tt.append(0); tv.append(0); 
+            else: tt.append(labelsT[k]); tv.append(labelsV[k])
+            m += delta_s
+            n += delta_s
+    ZT, ZV = np.asarray(ZT), np.asarray(ZV)
+    tt, tv = np.asarray(tt), np.asarray(tv)
     
-    # step = (fh-fl) / (nbands+1) # n_bins/setup['nbands']+1
-    # size = step / 0.5 # step/overlap
-    # sub_bands = []
-    # for i in range(nbands):
-    #     fl_sb = i * step + fl
-    #     fh_sb = i * step + size + fl
-    #     sub_bands.append([fl_sb, fh_sb])
+    step = (fh-fl) / (nbands+1) # n_bins/setup['nbands']+1
+    size = step / 0.5 # step/overlap
+    sub_bands = []
+    for i in range(nbands):
+        fl_sb = i * step + fl
+        fh_sb = i * step + size + fl
+        sub_bands.append([fl_sb, fh_sb])
         
-    # XT, XV = [], []
-    # if filtering['design'] == 'DFT':
-    #     filt = Filter(fl, fh, Fs, filtering)
-    #     XTF = filt.apply_filter(ZT)
-    #     XVF = filt.apply_filter(ZV)
-    #     for i in range(nbands):
-    #         bsize = 2 / ( Fs / ZT.shape[-1] ) # 2 representa sen e cos
-    #         XT.append(XTF[:, :, round(sub_bands[i][0]*bsize):round(sub_bands[i][1]*bsize)])
-    #         bsize = 2 / ( Fs / ZV.shape[-1] )
-    #         XV.append(XVF[:, :, round(sub_bands[i][0]*bsize):round(sub_bands[i][1]*bsize)])         
-    # elif filtering['design'] == 'IIR':
-    #     for i in range(nbands):
-    #         filt = Filter(sub_bands[i][0], sub_bands[i][1], Fs, filtering)
-    #         XT.append(filt.apply_filter(ZT))
-    #         XV.append(filt.apply_filter(ZV))
+    XT, XV = [], []
+    if filtering['design'] == 'DFT':
+        filt = Filter(fl, fh, Fs, filtering)
+        XTF = filt.apply_filter(ZT)
+        XVF = filt.apply_filter(ZV)
+        for i in range(nbands):
+            bsize = 2 / ( Fs / ZT.shape[-1] ) # 2 representa sen e cos
+            XT.append(XTF[:, :, round(sub_bands[i][0]*bsize):round(sub_bands[i][1]*bsize)])
+            bsize = 2 / ( Fs / ZV.shape[-1] )
+            XV.append(XVF[:, :, round(sub_bands[i][0]*bsize):round(sub_bands[i][1]*bsize)])         
+    elif filtering['design'] == 'IIR':
+        for i in range(nbands):
+            filt = Filter(sub_bands[i][0], sub_bands[i][1], Fs, filtering)
+            XT.append(filt.apply_filter(ZT))
+            XV.append(filt.apply_filter(ZV))
             
-    # csp = [ CSP(n_components=ncsp) for i in range(nbands) ] # mne.decoding.CSP
-    # for i in range(nbands): csp[i].fit(XT[i], tt)
-    # FT = [ csp[i].transform(XT[i]) for i in range(nbands) ]
-    # FV = [ csp[i].transform(XV[i]) for i in range(nbands) ]
+    csp = [ CSP(n_components=ncsp) for i in range(nbands) ] # mne.decoding.CSP
+    for i in range(nbands): csp[i].fit(XT[i], tt)
+    FT = [ csp[i].transform(XT[i]) for i in range(nbands) ]
+    FV = [ csp[i].transform(XV[i]) for i in range(nbands) ]
     
-    # FT = np.vstack(np.transpose(FT, (0,2,1))).T    
-    # FV = np.vstack(np.transpose(FV, (0,2,1))).T 
-    # # FT = normalize(FT, norm='l2')
-    # # FV = normalize(FV, norm='l2')
+    FT = np.vstack(np.transpose(FT, (0,2,1))).T    
+    FV = np.vstack(np.transpose(FV, (0,2,1))).T 
+    # FT = normalize(FT, norm='l2')
+    # FV = normalize(FV, norm='l2')
     
-    # lda.fit(FT, tt)
-    # y, yp = lda.predict(FV), lda.predict_proba(FV)
-    # print('On LDA acc:', round(lda.score(FV, tv)*100,2))
+    lda.fit(FT, tt)
+    y, yp = lda.predict(FV), lda.predict_proba(FV)
+    print('On LDA acc:', round(lda.score(FV, tv)*100,2))
      
-    # TT, TV = np.zeros((len(ZT), 3)), np.zeros((len(ZV), 3))
-    # for i in range(3): TT[:,i] = np.where(tt == i, 1, TT[:,i])
-    # for i in range(3): TV[:,i] = np.where(tv == i, 1, TV[:,i])
+    TT, TV = np.zeros((len(ZT), 3)), np.zeros((len(ZV), 3))
+    for i in range(3): TT[:,i] = np.where(tt == i, 1, TT[:,i])
+    for i in range(3): TV[:,i] = np.where(tv == i, 1, TV[:,i])
     
-    # # mlp = MLPClassifier(hidden_layer_sizes=(10,2), max_iter=10000, activation='tanh', tol=1e-4, verbose=False) #, random_state=42)
-    # # mlp.out_activation = 'softmax' # 'logistic', 'softmax'
-    # mlp.fit(FT, TT)
-    # y, yp = mlp.predict(FV), mlp.predict_proba(FV)
-    # print('On MLP acc:', round(mlp.score(FV, TV)*100,2), '\n') 
-    # # print(confusion_matrix(tv, y))
+    # mlp = MLPClassifier(hidden_layer_sizes=(10,2), max_iter=10000, activation='tanh', tol=1e-4, verbose=False) #, random_state=42)
+    # mlp.out_activation = 'softmax' # 'logistic', 'softmax'
+    mlp.fit(FT, TT)
+    y, yp = mlp.predict(FV), mlp.predict_proba(FV)
+    print('On MLP acc:', round(mlp.score(FV, TV)*100,2), '\n') 
+    # print(confusion_matrix(tv, y))
